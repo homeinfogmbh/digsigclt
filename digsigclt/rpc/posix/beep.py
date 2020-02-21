c5433e373a810ca0241b20e37ea35b335bb95054
Richard Neumann <r.neumann@homeinfo.de>
2020-02-21 15:08:33 +0100

    Refac.

--- digsigclt/rpc/posix/beep.py
+++ digsigclt/rpc/posix/beep.py
@@ -6,12 +6,11 @@ from subprocess import check_call
 __all__ = ['beep']
 
 
+BEEP = '/usr/bin/beep'
+
+
 def beep(args=None):
     """Performs a speaker beep to identify the system."""
 
-    command = ['/usr/bin/beep']
-
-    if args:
-        command += args
-
+    command = (BEEP, *args) if args else BEEP
     return check_call(command)
