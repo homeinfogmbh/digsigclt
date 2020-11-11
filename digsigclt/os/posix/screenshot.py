0d98e05a65967021aca1123a80d2695f7d91d683
Richard Neumann <r.neumann@homeinfo.de>
2020-11-11 11:29:50 +0100

    Fixing screenshot taking.

--- digsigclt/os/posix/screenshot.py
+++ digsigclt/os/posix/screenshot.py
@@ -1,7 +1,8 @@
 """Takes screenshots."""
 
+from pathlib import Path
 from subprocess import check_call
-from tempfile import NamedTemporaryFile
+from tempfile import TemporaryDirectory
 
 from digsigclt.os.posix.common import SCROT
 from digsigclt.types import Screenshot
@@ -11,20 +12,20 @@ __all__ = ['screenshot']
 
 
 FORMATS = {
-    '.jpe': 'image/jpeg',
-    '.jpeg': 'image/jpeg',
-    '.jpg': 'image/jpeg',
-    '.png': 'image/png',
-    '.gif': 'image/gif'
+    'jpe': 'image/jpeg',
+    'jpeg': 'image/jpeg',
+    'jpg': 'image/jpeg',
+    'png': 'image/png',
+    'gif': 'image/gif'
 }
 
 
-def screenshot(suffix: str = '.jpg', display: str = ':0',
+def screenshot(filetype: str = 'jpg', display: str = ':0',
                quality: int = None, multidisp: bool = False) -> Screenshot:
     """Takes a screenshot."""
 
     try:
-        content_type = FORMATS[suffix]
+        content_type = FORMATS[filetype]
     except KeyError:
         raise ValueError('Invalid image file type.') from None
 
@@ -36,9 +37,10 @@ def screenshot(suffix: str = '.jpg', display: str = ':0',
     if multidisp:
         command.append('--multidisp')
 
-    with NamedTemporaryFile('w+b', suffix=suffix) as file:
-        command.append(file.name)
+    with TemporaryDirectory() as tmpd:
+        tmpfile = Path(tmpd).joinpath(f'digsigclt-screenshot.{filetype}')
+        command.append(str(tmpfile))
         check_call(command)
-        file.flush()
-        file.seek(0)
-        return Screenshot(file.read(), content_type)
+
+        with tmpfile.open('rb') as file:
+            return Screenshot(file.read(), content_type)
