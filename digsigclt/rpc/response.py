b29e5a062a8f77a08d701794597bc15a40a67e48
Richard Neumann <mail@richard-neumann.de>
2020-12-02 01:23:38 +0100

    Refac.

--- digsigclt/rpc/response.py
+++ digsigclt/rpc/response.py
@@ -12,13 +12,13 @@ __all__ = ['Response']
 
 ERRORS = {
     ValueError: lambda error: (', '.join(error.args), 400),
-    NotImplementedError: lambda error: (
-        'Beeping is not implemented on this platform.', 501),
+    NotImplementedError:
+        lambda error: ('Beeping is not implemented on this platform.', 501),
     CalledProcessError: lambda error: (str(error), 500),
-    UnderAdministration: lambda error: (
-        'The system is currently under administration.', 503),
-    PackageManagerActive: lambda error: (
-        'The package manager is currently running.', 503)
+    UnderAdministration:
+        lambda error: ('The system is currently under administration.', 503),
+    PackageManagerActive:
+        lambda error: ('The package manager is currently running.', 503)
 }
 
 
