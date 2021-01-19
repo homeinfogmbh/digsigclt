09d4db44f8a2727d0915db601d31bc2bc75e5b0c
Richard Neumann <r.neumann@homeinfo.de>
2021-01-19 11:29:31 +0100

    Added test case for copy_file().

--- tests/test_common.py
+++ tests/test_common.py
@@ -2,9 +2,10 @@
 
 from json import dumps
 from pathlib import Path
+from tempfile import TemporaryFile
 from unittest import TestCase
 
-from digsigclt.common import FileInfo
+from digsigclt.common import FileInfo, copy_file
 
 
 CONTENT = 'Hello, world.\n'
@@ -12,6 +13,8 @@ PATH = Path(__file__).parent.joinpath('testfile.txt')
 CTIME = 1611050058.40735
 SHA256 = '1ab1a2bb8502820a83881a5b66910b819121bafe336d76374637aa4ea7ba2616'
 JSON = {'sha256sum': SHA256, 'ctime': CTIME}
+CHUNK_SIZE = 1024
+SIZE = CHUNK_SIZE ** 2
 
 
 class TestFileInfo(TestCase):
@@ -37,3 +40,17 @@ class TestFileInfo(TestCase):
         """Tests the FileInfo.from_file classmethod."""
         self._test_from_file_str()
         self._test_from_file_path()
+
+
+class TestCopyFile(TestCase):
+    """Tests the copy_file() function."""
+
+    def test_copy_file(self):
+        """Tests the copy_file() function."""
+        with open('/dev/zero', 'rb') as src, TemporaryFile('w+b') as dst:
+            copy_file(src, dst, SIZE, chunk_size=CHUNK_SIZE)
+            dst.flush()
+            dst.seek(0)
+            size = len(dst.read())
+
+        self.assertEqual(size, SIZE)
