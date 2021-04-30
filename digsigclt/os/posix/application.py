751fe68855e027a1694301c93f3f335bb7fecc54
Richard Neumann <mail@richard-neumann.de>
2021-04-30 10:58:12 +0200

    Add control over HTML5 application

--- digsigclt/os/posix/application.py
+++ digsigclt/os/posix/application.py
@@ -9,37 +9,48 @@ from digsigclt.types import ServiceState
 __all__ = ['enable', 'disable', 'status']
 
 
-SERVICE = 'application.service'
-ENABLE = sudo(systemctl('enable', '--now', SERVICE))
-DISABLE = sudo(systemctl('disable', '--now', SERVICE))
-CHECK_ENABLED = systemctl('is-enabled', SERVICE)
-CHECK_RUNNING = systemctl('is-active', SERVICE)
+SERVICE_AIR = 'application.service'
+SERVICE_HTML = 'html5ds.service'
+
+
+def get_service() -> str:
+    """Returns the service."""
+
+    try:
+        check_call(systemctl('status', SERVICE_HTML))
+    except CalledProcessError as cpe:
+        if cpe.returncode == 4:     # HTML version not installed.
+            return SERVICE_AIR
+
+    return SERVICE_HTML
 
 
 def enable() -> int:
     """Enables the digital signage application."""
 
-    return check_call(ENABLE)
+    return check_call(sudo(systemctl('enable', '--now', get_service())))
 
 
 def disable() -> int:
     """Disables the digital signage application."""
 
-    return check_call(DISABLE)
+    return check_call(sudo(systemctl('disable', '--now', get_service())))
 
 
 def status() -> ServiceState:
     """Enables the digital signage application."""
 
+    service = get_service()
+
     try:
-        check_call(CHECK_ENABLED)
+        check_call(systemctl('is-enabled', service))
     except CalledProcessError:
         enabled = False
     else:
         enabled = True
 
     try:
-        check_call(CHECK_RUNNING)
+        check_call(systemctl('is-active', service))
     except CalledProcessError:
         running = False
     else:
